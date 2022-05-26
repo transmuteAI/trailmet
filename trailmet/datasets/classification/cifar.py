bf01824284407b61a2463ebc90a24f7bbe5f0a94
dkgupta90 <deepauck@gmail.com>
2022-05-26 08:35:18 +0400

    started adding dataset related template for base code

---
+++ trailmet/datasets/classification/cifar.py
@@ -0,0 +1,19 @@
+
+import os
+
+from ..dataset import Dataset
+
+class CIFAR10Dataset(Dataset):
+
+    def __init__(self):
+        super(CIFAR10Dataset, self).__init__()
+        pass
+
+class CIFAR100Dataset(Dataset):
+
+    def __init__(self):
+        super(CIFAR100Dataset, self).__init__()
+        pass
+
+
+
