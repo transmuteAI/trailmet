92af8dd3c9d2dff3f1bf3ce7ca6e7aea0d5ef2ca
piyu2181 <36099241+piyu2181@users.noreply.github.com>
2022-06-05 11:50:15 +0400

    initial commit

---
+++ trailmet/algorithms/algorithms.py
@@ -0,0 +1,16 @@
+class Algorithms:
+
+    def __init__(self):
+        pass
+
+    def compress_model(self):
+        pass
+
+    def set_hyperparams(device=None,
+                        dataset=None,
+                        optimizer='SGD',
+                        scheduler=None,
+                        ):
+        self.optimizer = optimizer
+        pass
+
