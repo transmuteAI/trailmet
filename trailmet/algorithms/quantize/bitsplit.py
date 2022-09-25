c5243d5c2c847b3b651cfe417ae52a970d6c1721
heisenbug237 <areebsyed237@gmail.com>
2022-09-25 21:04:04 +0530

    updated base

--- trailmet/algorithms/quantize/bitsplit.py
+++ trailmet/algorithms/quantize/bitsplit.py
@@ -1,6 +1,6 @@
 
 
-
+import os
 import torch
 import copy
 import torch.nn as nn
@@ -23,6 +23,11 @@ class BitSplit(BaseQuantization):
         self.device = torch.device('cuda:{}'.format(self.gpu_id))
         torch.cuda.set_device(self.gpu_id)
         seed_everything(self.seed)
+        self.arch = self.kwargs.get('ARCH', "ResNet50")
+        self.prefix = self.arch + '/A'+str(self.a_bits)+'W'+str(self.w_bits)
+        if not os.path.exists(self.prefix):
+            os.makedirs(self.prefix)
+        self.scales = False
 
         
     def compress_model(self):
@@ -37,10 +42,305 @@ class BitSplit(BaseQuantization):
                 self.act_quant_modules.append(m)
         self.act_quant_modules[-1].set_bitwidth(8)
 
-
+        #############################
         #### Weight Quantization ####
+        #############################
         conv = self.model.conv1
         q_conv = self.qmodel.conv1
-        # conduct
+        conduct_ofwa(self.train_loader, self.model, self.qmodel, conv, q_conv, None, 8,
+                     prefix=self.prefix+'/fc', device=self.device, ec=False)
+        #### quantize 4 blocks
+        for layer_idx in range(1, 5):
+            current_layer_pretrained = eval('self.model.layer{}'.format(layer_idx))
+            current_layer_quan = eval('self.qmodel.layer{}'.format(layer_idx))
+            for block_idx in range(len(current_layer_pretrained)):
+                current_block_pretrained = current_layer_pretrained[block_idx]
+                current_block_quan = current_layer_quan[block_idx]
+                pkl_path = self.prefix+'/layer'+str(layer_idx)+'_block'+str(block_idx)
+                # conv1
+                conv = current_block_pretrained.conv1
+                conv_quan = current_block_quan.conv1
+                q_module = current_block_quan.quan1
+                conduct_ofwa(self.train_loader, self.model, self.qmodel, conv, conv_quan, q_module, 
+                            self.w_bits, prefix=pkl_path+'_conv1', device=self.device, ec=False)
+                # conv2
+                conv = current_block_pretrained.conv2
+                conv_quan = current_block_quan.conv2
+                q_module = current_block_quan.quan2
+                conduct_ofwa(self.train_loader, self.model, self.qmodel, conv, conv_quan, q_module, 
+                            self.w_bits, prefix=pkl_path+'_conv2', device=self.device, ec=False)
+                # downsample
+                if current_block_pretrained.downsample is not None:
+                    conv = current_block_pretrained.downsample[0]
+                    conv_quan = current_block_quan.downsample[0]
+                    conduct_ofwa(self.train_loader, self.model, self.qmodel, conv, conv_quan, None, 
+                                self.w_bits, prefix=pkl_path+'_downsample', device=self.device, ec=False)
+        ## quantize last fc
+        conv = self.model.fc
+        conv_quan = self.qmodel.fc
+        q_module = self.qmodel.quan
+        conduct_ofwa(self.train_loader, self.model, self.qmodel, conv, conv_quan, q_module, 8, 
+                     prefix=self.prefix+'/fc', device=self.device, ec=False)
+        
+        ##################################
+        #### Load Weight Quantization ####
+        ##################################
+        conv = self.model.conv1
+        conv_quan = self.qmodel.conv1
+        load_ofwa(conv, conv_quan, None, 8, prefix=self.prefix+'/conv1')
+        for layer_idx in range(1, 5):
+            current_layer_pretrained = eval('self.model.layer{}'.format(layer_idx))
+            current_layer_quan = eval('self.qmodel.layer{}'.format(layer_idx))
+            for block_idx in range(len(current_layer_pretrained)):
+                current_block_pretrained = current_layer_pretrained[block_idx]
+                current_block_quan = current_layer_quan[block_idx]
+                # conv1
+                conv = current_block_pretrained.conv1
+                conv_quan = current_block_quan.conv1
+                q_module = current_block_quan.quan1
+                load_ofwa(conv, conv_quan, q_module, self.w_bits, prefix=self.prefix+'/layer'+str(layer_idx)+'_block'+str(block_idx)+'_conv1')
+                # conv2
+                conv = current_block_pretrained.conv2
+                conv_quan = current_block_quan.conv2
+                q_module = current_block_quan.quan2
+                load_ofwa(conv, conv_quan, q_module, self.w_bits, prefix=self.prefix+'/layer'+str(layer_idx)+'_block'+str(block_idx)+'_conv2')
+                # downsample
+                if current_block_pretrained.downsample is not None:
+                    conv = current_block_pretrained.downsample[0]
+                    conv_quan = current_block_quan.downsample[0]
+                    load_ofwa(conv, conv_quan, None, self.w_bits, prefix=self.prefix+'/layer'+str(layer_idx)+'_block'+str(block_idx)+'_downsample')
+        conv = self.model.fc
+        conv_quan = self.model.fc
+        q_module = self.qmodel.quan
+        load_ofwa(conv, conv_quan, q_module, 8, prefix=self.prefix+'/fc')
+        
+        #################################
+        #### Activation Quantization ####
+        #################################
+        print("quantizing ('{}-bit')...".format(self.a_bits))
+        # update(train_loader, model_quan, criterion, args, 200)
+        if self.scales:
+            scales = np.load(self.scales)
+            # enable feature map quantization
+            for index, q_module in enumerate(self.act_quant_modules):
+                q_module.set_scale(scales[index])
+        else:
+            quantize(self.train_loader, self.qmodel, self.a_bits, self.act_quant_modules,
+                    device=self.device, prefix=self.prefix)
+
+        # print('update ...')
+        # update(train_loader, model_quan, criterion, args, 200)
+        # print('validate quantization...')
+        # validate(val_loader, model_quan, criterion, args)
+
+        save_state_dict(self.qmodel.state_dict(), self.prefix, filename='state_dict.pth')
 
 
+
+import numpy as np
+import random
+import pickle
+from collections import OrderedDict
+from bitsplit_dump import ofwa, ofwa_rr
+
+global feat, prev_feat, conv_feat
+def hook(module, input, output):
+    global feat
+    feat = output.data.cpu().numpy()
+def current_input_hook(module, inputdata, outputdata):
+    global prev_feat
+    prev_feat = inputdata[0].data#.cpu()#.numpy()
+def conv_hook(module, inputdata, outputdata):
+    global conv_feat
+    conv_feat = outputdata.data#.cpu()#.numpy()
+
+
+def conduct_ofwa(train_loader, model_pretrained, model_quan, conv, conv_quan, 
+                q_module, bitwidth, device, prefix=None, ec=False):
+    # for fc
+    if not hasattr(conv, 'kernel_size'):
+        W = conv.weight.data#.cpu()
+        W_shape = W.shape
+        B_sav, B, alpha = ofwa(W.cpu().numpy(), bitwidth)
+        with open(prefix + '_fwa.pkl', 'wb') as f:
+            pickle.dump({'B': B, 'alpha': alpha}, f, pickle.HIGHEST_PROTOCOL)
+        if ec:
+            W_r = np.multiply(B, np.expand_dims(alpha, 1)).reshape(W_shape)
+            conv_quan.weight.data.copy_(torch.from_numpy(W_r))
+        return
+
+    # conv parameters
+    kernel_h, kernel_w = conv.kernel_size
+    pad_h, pad_w = conv.padding
+    stride_h, stride_w = conv.stride
+
+    handle_prev = conv_quan.register_forward_hook(current_input_hook)
+    handle_conv = conv.register_forward_hook(conv_hook)
+
+    batch_iterator = iter(train_loader)
+
+    # weights and bias
+    W = conv.weight.data#.cpu()
+    if conv.bias is None:
+        bias = torch.zeros(W.shape[0]).to(conv.weight.device)
+    else:
+        bias = conv.bias.data#.cpu()
+    print(W.shape)
+
+    # feat extract
+    n_batches = 30
+    per_batch = 400
+    input, target = next(batch_iterator)
+    input_pretrained = input.cuda(device=device, non_blocking=True)
+    input_quan = input.cuda(device=device, non_blocking=True)
+    model_pretrained(input_pretrained)
+    model_quan(input_quan)
+    # print(prev_feat.shape)
+    # print(conv_feat.shape)
+    [prev_feat_n, prev_feat_c, prev_feat_h, prev_feat_w] = prev_feat.shape
+    [conv_feat_n, conv_feat_c, conv_feat_h, conv_feat_w] = conv_feat.shape
+
+    X = torch.zeros(n_batches*per_batch, prev_feat_c, kernel_h, kernel_w).to(device)
+    Y = torch.zeros(n_batches*per_batch, conv_feat_c).to(device)
+    print(X.shape)
+    print(Y.shape)
+
+    for batch_idx in range(0, n_batches):
+        input, target = next(batch_iterator)
+        input_pretrained = input.cuda(device=device, non_blocking=True)
+        model_pretrained(input_pretrained)
+        input_quan = input.cuda(device=device, non_blocking=True)
+        model_quan(input_quan)
+    
+        prev_feat_pad = torch.zeros(prev_feat_n, prev_feat_c, prev_feat_h+2*pad_h, prev_feat_w+2*pad_w).to(device)
+        prev_feat_pad[:, :, pad_h:pad_h+prev_feat_h, pad_w:pad_w+prev_feat_w] = prev_feat
+        prev_feat_pad = prev_feat_pad.unfold(2, kernel_h, stride_h).unfold(3, kernel_w, stride_w).permute(0,2,3,1,4,5)
+        [feat_pad_n, feat_pad_h, feat_pad_w, feat_pad_c, feat_pad_hh, feat_pad_ww] = prev_feat_pad.shape
+        assert(feat_pad_hh==kernel_h)
+        assert(feat_pad_ww==kernel_w)
+        # prev_feat_pad = prev_feat_pad.reshape(feat_pad_n*feat_pad_h*feat_pad_w, -1)
+        prev_feat_pad = prev_feat_pad.reshape(feat_pad_n*feat_pad_h*feat_pad_w, feat_pad_c, kernel_h, kernel_w)
+        rand_index = list(range(prev_feat_pad.shape[0]))
+        random.shuffle(rand_index)
+        rand_index = rand_index[0:per_batch]
+        X[per_batch*batch_idx:per_batch*(batch_idx+1),:] = prev_feat_pad[rand_index, :]
+        conv_feat_tmp = conv_feat.permute(0,2,3,1).reshape(-1, conv_feat_c) - bias
+        Y[per_batch*batch_idx:per_batch*(batch_idx+1),:] = conv_feat_tmp[rand_index, :]
+    
+    handle_prev.remove()
+    handle_conv.remove()
+    
+    ## ofwa init
+    W_shape = W.shape
+    X = X.cpu().numpy()
+    Y = Y.cpu().numpy()
+    W = W.reshape(W_shape[0], -1)
+    B_sav, B, alpha = ofwa(W.cpu().numpy(), bitwidth)
+    B, alpha = ofwa_rr(X, Y, B_sav, alpha, bitwidth, max_epoch=100)
+    with open(prefix + '_rr_b30x400_e100.pkl', 'wb') as f:
+        pickle.dump({'B': B, 'alpha': alpha}, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_ofwa(conv, conv_quan, q_module, bitwidth, prefix=None):
+    # for fc
+    if not hasattr(conv, 'kernel_size'):
+        W = conv.weight.data#.cpu()
+        W_shape = W.shape
+        with open(prefix + '_fwa.pkl', 'rb') as f:
+            B_alpha = pickle.load(f)
+            B = B_alpha['B']
+            alpha = B_alpha['alpha']
+        W_r = np.multiply(B, np.expand_dims(alpha, 1)).reshape(W_shape)
+        conv_quan.weight.data.copy_(torch.from_numpy(W_r))
+        return
+
+    # weights and bias
+    W = conv.weight.data#.cpu()
+    W_shape = W.shape
+
+    with open(prefix + '_rr_b30x400_e100.pkl', 'rb') as f:
+        B_alpha = pickle.load(f)
+        B = B_alpha['B']
+        alpha = B_alpha['alpha']
+    W_r = np.multiply(B, np.expand_dims(alpha, 1)).reshape(W_shape)
+    conv_quan.weight.data.copy_(torch.from_numpy(W_r))
+
+
+def save_state_dict(state_dict, path, filename='state_dict.pth'):
+    saved_path = os.path.join(path, filename)
+    new_state_dict = OrderedDict()
+    for key in state_dict.keys():
+        if '.module.' in key:
+            new_state_dict[key.replace('.module.', '.')] = state_dict[key].cpu()
+        else:
+            new_state_dict[key] = state_dict[key].cpu()
+    torch.save(new_state_dict, saved_path)
+
+
+def quantize(trainloader, model, a_bits, act_quant_modules, device, prefix):
+    
+    def get_safelen(x):
+        x = x / 10
+        y = 1
+        while(x>=10):
+            x = x / 10
+            y = y * 10
+        return int(y)
+
+    act_sta_len = 3000000
+    feat_buf = np.zeros(act_sta_len)
+    scales = np.zeros(len(act_quant_modules))
+
+    with torch.no_grad():
+        for index, q_module in enumerate(act_quant_modules):
+            # batch_iterator = iter(torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True,
+                                                            #  num_workers=args.workers, pin_memory=True))
+            batch_iterator = iter(trainloader)
+            images, targets = next(batch_iterator)
+            images = images.cuda()
+            targets = targets.cuda()
+
+            #### ADD HANDLE ####
+            handle = q_module.register_forward_hook(hook)
+            model(images)
+
+            #global feat
+            feat_len = feat.size
+            per_batch = min(get_safelen(feat_len), 100000)
+            n_batches = int(act_sta_len / per_batch)
+
+            failed = True
+            while(failed):
+                failed = False
+                print('Extracting features for ', n_batches, ' batches...')
+                for batch_idx in range(0, n_batches):
+                    images, targets = next(batch_iterator)
+                    images = images.cuda(device=device, non_blocking=True)
+                    # forward
+                    model(images)
+
+                    #global feat
+                    if q_module.signed:
+                        feat_tmp = np.abs(feat).reshape(-1)
+                    else:
+                        feat_tmp = feat[feat>0].reshape(-1)
+                        if feat_tmp.size < per_batch:
+                            per_batch = int(per_batch / 10)
+                            n_batches = int(n_batches * 10)
+                            failed = True
+                            break
+                    np.random.shuffle(feat_tmp)
+                    feat_buf[batch_idx*per_batch:(batch_idx+1)*per_batch] = feat_tmp[0:per_batch]
+
+                if(not failed):
+                    print('Init quantization... ')
+                    scales[index] = q_module.init_quantization(feat_buf)
+                    print(scales[index])
+                    np.save(os.path.join(prefix, 'act_' + str(a_bits) + '_scales.npy'), scales)
+            #### REMOVE HANDLE ####
+            handle.remove()
+
+    np.save(os.path.join(prefix, 'act_' + str(a_bits) + '_scales.npy'), scales)
+    # enable feature map quantization
+    for index, q_module in enumerate(act_quant_modules):
+        q_module.set_scale(scales[index])
---
+++ trailmet/algorithms/quantize/bitsplit_dump.py
@@ -0,0 +1,200 @@
+
+import numpy as np
+
+
+def split(Q, bitwidth):
+    """
+    Split low-bit weight integers into a list of ternary weights.
+    """
+    Q_sign = np.sign(Q)
+    Q_abs = np.abs(Q)
+    B_sav = []
+    for idx in range(bitwidth-1):
+        B = (Q_abs - Q_abs.astype(np.int)//2*2) # get current last bit
+        B *= Q_sign
+        B_sav.append(B)
+        Q_abs = (Q_abs.astype(np.int)//2).astype(np.float32) # Q_abs >> 1
+
+    return B_sav[::-1]
+
+def get_int_B(B_sav):
+    """
+    Stitch a list of ternary vectors into a integer vectors.
+    """
+    B_sum = B_sav[0].copy()
+    for idx in range(1, len(B_sav)):
+        B_sum += B_sav[idx] / (2**idx)
+    return B_sum
+
+def get_int_B_exclusive(B_sav, bit):
+    """
+    Stitch a list of ternary vectors into a integer vectors. The i-th bit is excluded.
+    """
+    # NOTE: the position of the decimal point is not at the end. E.g. 4-bit fixed-point numbers could be something like:
+    # +1.01, -0.11, +1.10, ... instead of +101, -011, +110, ... .
+    mask = [1.0]*len(B_sav)
+    mask[bit] = 0
+    B_sum = B_sav[0] * mask[0]
+    for idx in range(1, len(B_sav)):
+        B_sum += B_sav[idx] * mask[idx] / (2**idx)
+    return B_sum
+
+def get_ot_given_a(W, alpha):
+    """
+    Get the optimal ternary vector, given the quantization scale alpha
+    """
+    [m, n] = W.shape
+    B=W.copy()
+    B=(B>=0).astype(np.float32)*2-1
+    abs_W2 = np.abs(W) * 2
+    B[abs_W2<alpha[:, np.newaxis]] = 0
+
+    return B
+
+
+
+def fwa(W, bitwidth):
+    """
+    Fixed-point Weight Approximation. 
+    Minimize the MSE quantization error to find the optimal quantization scales
+    Given quantization scale, round-off is used for fixed-point quantization
+    """
+    max_val = 2**(bitwidth-1) - 1
+    alpha = np.abs(W).max(axis=1) / max_val
+    alpha_old = alpha*1.1
+    while(np.linalg.norm(alpha-alpha_old)>1e-9):
+        q = W / alpha[:, np.newaxis]
+        q = np.round(q)
+        q = np.clip(q, -max_val, max_val)
+        alpha_old = alpha
+        alpha = np.sum(W*q, axis=1) / np.sum(q*q, axis=1)
+    return q, alpha
+
+def ofwa(W, bitwidth, max_epoch=50):
+    """
+    Optimal Fixed Point Weight Approximation Method.
+    Minimize weight matrix reconstruction error using bit-split strategy.
+    Given quantization scale, we find the 'optimal' low-bit weights that 
+    minimizes the weight quantization error (instead of using round-off).
+    Initialized by "fwa".
+    """
+    assert(bitwidth >= 3)
+    Q, alpha = fwa(W, bitwidth)
+    B_sav = split(Q, bitwidth)
+    # NOTE: the position of the decimal point is not at the end. 
+    # E.g. 4-bit fixed-point numbers could be something like:
+    # +1.01, -0.11, +1.10, ... instead of +101, -011, +110, ... .
+    alpha *= (2**(bitwidth-2))  
+
+    ### iterative optimization
+    [m, n] = W.shape
+    for _ in range(max_epoch):
+        # print(_)
+        alpha_old = np.copy(alpha)
+        B_sum = get_int_B(B_sav)
+        # given Ws, optimize alpha
+        alpha = np.sum(W*B_sum, axis=1) / np.sum(B_sum*B_sum, axis=1)
+
+        if np.linalg.norm(alpha_old-alpha) <= 1e-9:
+            break
+        # given alpha, optimize Ws
+        for bit in range(bitwidth-1):
+            W_res = W - get_int_B_exclusive(B_sav, bit) * alpha[:, np.newaxis]
+            B = get_ot_given_a(W_res*(2**bit), alpha)
+            B_sav[bit] = B
+
+    B_sum = get_int_B(B_sav)
+
+    return B_sav, B_sum, alpha
+
+def ofwa_rr(X, Y, B_sav, alpha, bitwidth, max_epoch=100):
+    """
+    Optimal Fixed Point Weight Approximation with Response Reconstruction.
+    Minimize activation matrix reconstruction error using bit-split strategy.
+    Initialized by "ofwa".
+    :X: K,C,d,d
+    :Y: K,M
+    :B: M,N    M kernels
+    objective:
+    min(Y-XWA)^2
+    """
+    # X: K,N   (N=C*d*d)
+    X = X.reshape(X.shape[0], -1)
+    K, N = X.shape
+
+    A = np.dot(X.T, X) # N,N
+
+    for epoch in range(max_epoch):
+        # given Bi, optimize alpha
+        B_sum = get_int_B(B_sav)
+        XB = np.dot(X, B_sum.T) # k,m
+        alpha = np.einsum("ij,ij->j", Y, XB)
+        alpha = alpha / np.einsum("ij,ij->j", XB, XB)
+
+        # given alpha, optimize Bi
+        for bit in range(bitwidth-1):
+            B = B_sav[bit]
+            B_others = get_int_B_exclusive(B_sav, bit) * alpha[:, np.newaxis]
+            Y_res = Y - np.dot(X, B_others.T)
+
+            T = np.dot(Y_res.T, X) # M,N
+            ## fix alpha, optimize B
+            # parallel degree: M
+            for n in range(N):
+                B[:, n] = 0
+                ABn = np.dot(A[n], B.T)
+                lump = 2 * (ABn * (alpha/(2**bit))- T[:, n]) # M
+                B[:, n] = -np.sign(lump)
+                B[np.abs(lump) < (alpha/(2**bit)) * A[n,n], n] = 0
+
+    B_sum = get_int_B(B_sav)
+
+    return B_sum, alpha
+
+def ofwa_rr_dw(X, Y, B_sav, alpha, bitwidth, max_epoch=100):
+
+    '''
+    # X: K,M,d,d
+    # Y: K,M
+    # B: M,N    M kernels
+    objective:
+    min(Y-XWA)^2
+    '''
+    # X: M,K,9   (N=d*d)
+    X = np.transpose(X.reshape(X.shape[0], X.shape[1], -1), (1, 0, 2)) # M, K, 9
+    As = np.matmul(np.transpose(X, (0, 2, 1)), X) # M, 9, 9
+
+    alpha_bk = alpha
+    for epoch in range(max_epoch):
+        # given Bi, optimize alpha
+        B_sum = get_int_B(B_sav)
+        XB = np.matmul(X, np.expand_dims(B_sum, axis=2)) # M, K, 1
+        XB = np.squeeze(XB, axis=2) # M, K
+        XB = XB.T
+
+        alpha = np.einsum("ij,ij->j", Y, XB)
+        alpha = alpha / np.einsum("ij,ij->j", XB, XB)
+        nan_pos = np.isnan(alpha)
+        alpha[nan_pos] = alpha_bk[nan_pos]
+
+        # given alpha, optimize Bi
+        for bit in range(bitwidth-1):
+            B = B_sav[bit]
+            B_others = get_int_B_exclusive(B_sav, bit) * alpha[:, np.newaxis]
+            Y_res = Y - np.squeeze(np.matmul(X, np.expand_dims(B_others, axis=2)), axis=2).T # Y_res = Y - np.dot(X, B_others.T)
+
+            T = np.squeeze(np.matmul(np.expand_dims(Y_res.T, axis=1), X), axis=1) #T = np.dot(Y_res.T, X) # M,N
+            ## fix alpha, optimize B
+            # parallel degree: M
+            for n in range(9): # N=9
+                B[:, n] = 0
+                ABn = np.diagonal(np.dot(As[:,n], B.T)) # M #ABn = np.dot(A[n], B.T)
+                lump = 2 * (ABn * (alpha/(2**bit))- T[:, n]) # M
+                B[:, n] = -np.sign(lump)
+
+                B[np.abs(lump) < (alpha/(2**bit)) * As[:,n,n], n] = 0
+
+    B_sum = get_int_B(B_sav)
+
+    return B_sum, alpha
+    
