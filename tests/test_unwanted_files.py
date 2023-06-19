a114701076692901f5bb08f750123ae4a014241e
animesh-007 <animeshagarwal7@gmail.com>
2023-06-19 20:53:56 +0000

    added unit test for unwanted files

---
+++ tests/test_unwanted_files.py
@@ -0,0 +1,16 @@
+import os
+import re
+
+unwanted_extensions = ['.DS_Store', '.pyc', '.pth', '.gz']  # Add any other unwanted file extensions
+
+def test_unwanted_files():
+    root_dir = '..'  # Replace with the root directory of your repository
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        print(filenames)
+        print(dirpath)
+        if 'tests' in re.split(r'[\\/]', dirpath):
+            print(dirpath)
+            continue  # Skip the current directory
+        for filename in filenames:
+            file_extension = os.path.splitext(filename)[1]
+            assert file_extension not in unwanted_extensions, f"Unwanted file found: {filename}"
